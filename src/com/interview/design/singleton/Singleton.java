92b83208ad0128bef04faedb7e1a9dd8f9a286c7
meghz17 <meghmeg@gmail.com>
2017-01-29 23:43:47 -0600

    Added Singleton pattern

---
+++ src/com/interview/design/singleton/Singleton.java
@@ -0,0 +1,26 @@
+package com.interview.design.singleton;
+
+public class Singleton
+{
+	// Lazy Initialization
+	private static Singleton singletonInstance = null;
+
+	// Restriction of initialization from outside the class
+	private Singleton()
+	{
+		System.out.println("Singleton Instance created");
+	}
+
+	// Only exposed method to get an Instance
+	public static Singleton getInstance()
+	{
+		if (singletonInstance == null)
+		{
+			// Lazy initialization
+			singletonInstance = new Singleton();
+			return singletonInstance;
+		}
+
+		return null;
+	}
+}
